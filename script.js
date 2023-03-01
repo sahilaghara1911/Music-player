4be05fd12fa7e05cb0fda7e758a61ae9db8557b6
Sahil <nitinpatel9860@gmail.com>
2023-03-01 22:36:33 +0530

    add

---
+++ script.js
@@ -0,0 +1,68 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+const music = document.querySelector('audio');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
+// Music
+const songs = [
+    {
+        name : 'Starboy',
+        displayName : 'Starboy',
+        artist : 'The weekend',
+    },
+    {
+        name : 'Unstoppable',
+        displayName : 'Unstoppable',
+        artist : 'Sia',
+    },
+    {
+        name : 'See You Again',
+        displayName : 'See You Again',
+        artist : 'Wiz Khalifa',
+    },
+    {
+        name : 'Maroon5',
+        displayName : 'Animals ',
+        artist : 'Maroon5',
+    },
+    {
+        name : 'Legends Never Die',
+        displayName : 'Legends Never Die',
+        artist : 'Againts the current',
+    },
+    {
+        name : 'Famy_Ava',
+        displayName : 'Famy Ava',
+        artist : 'Famy Ava',
+    },
+    {
+        name : 'Eminem',
+        displayName : 'Mockingbird',
+        artist : 'Eminem',
+    },
+]
+
+// check if playing 
+let isPlaying = false;
+
+// Play
+function playSong() {
+    isPlaying = true;
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
+    music.play();
+}
+
+// Pause
+function pauseSong() {
+    isPlaying = false;
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+    music.pause();
+}
+
+// Play or Pause Event Listener
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
